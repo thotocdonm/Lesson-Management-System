d0a0223a6d65315e176661fccfda8f143748ca2a
Nguyen Minh Son <151738907+thotocdonm@users.noreply.github.com>
2024-12-05 14:11:56 +0700

    init

---
+++ QuanLyLichHoc/Views/MainForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace QuanLyLichHoc
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LessonBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
