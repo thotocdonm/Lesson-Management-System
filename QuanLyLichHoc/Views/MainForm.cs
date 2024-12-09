c039e1d13c717982069018e425d24bf5c8b6fed3
Nguyen Minh Son <151738907+thotocdonm@users.noreply.github.com>
2024-12-09 07:06:41 +0700

    update

--- QuanLyLichHoc/Views/MainForm.cs
+++ QuanLyLichHoc/Views/MainForm.cs
@@ -54,12 +54,12 @@ namespace QuanLyLichHoc
 
         }
 
-        private void SubjectsBtn_Click(object sender, EventArgs e)
+
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
         {
 
         }
-
-        private void panel2_Paint(object sender, PaintEventArgs e)
 
 
         private void SubjectsBtn_Click(object sender, EventArgs e)
